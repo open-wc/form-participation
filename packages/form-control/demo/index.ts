59b2a2c372758474160a5cd1324704e0d7233443
Caleb D. Williams <caleb.d.williams@gmail.com>
2023-10-01 14:36:29 -0500

    chore: modernize repo in preparation for full release

--- packages/form-control/demo/index.ts
+++ packages/form-control/demo/index.ts
@@ -1,3 +1,47 @@
 import './complex-demo';
 import './switch';
 import './async-validator-demo';
+import { css, html, LitElement } from 'lit';
+import { customElement, property, query } from 'lit/decorators.js';
+import { FormControlMixin, Validator } from '../src';
+
+@customElement('test-el')
+class TestEl extends FormControlMixin(LitElement) {
+  static styles = css`:host {display:block}`
+  static formControlValidators: Validator[] = [{
+    key: 'customError',
+    message: 'Oops',
+    isValid(instance: TestEl, value: string): boolean {
+      console.log(value === 'foo')
+      return value === 'foo'
+    }
+  }];
+
+  static shadowRootOptions: ShadowRootInit = {
+    mode: 'open',
+    delegatesFocus: true
+  }
+
+  @query('input')
+  validationTarget!: HTMLInputElement;
+
+  @property()
+  error = ''
+
+  firstUpdated() {
+    this.setValue('');
+    this.tabIndex = 0;
+  }
+
+  render() {
+    return html`<input @input=${this.onInput}>${this.error}`
+  }
+
+  onInput(event: Event & { target: HTMLInputElement }) {
+    this.setValue(event.target.value);
+  }
+
+  validationMessageCallback(message: string): void {
+    this.error = message;
+  }
+}
